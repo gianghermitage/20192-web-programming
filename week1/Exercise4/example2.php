cc1e22c4c1172414072db58bd89b217ae77ad971
Giang <trgiang1998@gmail.com>
2020-03-10 08:52:31 +0700

    commit week1 exercise

---
+++ week1/Exercise4/example2.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>
+            Generating HTML From PHP
+        </title>
+    </head>
+    <body>
+        <h1>Generating HTML From PHP</h1>
+        <?php
+        //
+        // Example script to output HTML tags
+        //
+        print("Using PHP has <i> some advantages: </i>");
+        print("<ul><li>Speed</li><li>Ease of use</li>"
+                . "<li>Functionality</li></ul>"); //output bullet list
+        print ("</body></html>");
+        ?>
