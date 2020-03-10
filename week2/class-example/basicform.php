d6e1fd340b49c54cfab1a140945f3c30dde56179
Giang <trgiang1998@gmail.com>
2020-03-10 11:08:43 +0700

    commit week2 lab example

---
+++ week2/class-example/basicform.php
@@ -0,0 +1,17 @@
+<html>
+    <head>
+        <title>
+            A simple form
+        </title>
+    </head>
+    <body>
+        <form action="test.php" method=POST>
+            click submit to start our initial PHP program.
+            <br>
+            <input type="text" name="name">
+            <br>
+            <input type="SUBMIT" value="Click to submit">
+            <input type="RESET" value="Erase and restart">
+        </form>
+    </body>
+</html>
