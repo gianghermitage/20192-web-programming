5c059b89de520e9924f74643a68f71f692a7af1a
Giang <trgiang1998@gmail.com>
2020-05-17 20:54:38 +0700

    done w9 + w10

--- week10/controller/Controller.php
+++ week10/controller/Controller.php
@@ -9,10 +9,12 @@
         }
         
         public function invoke(){
+            //if no book show all book
             if(!isset($_GET['book'])){
                 $books = $this->model->getBookList();
                 include 'view/booklist.php';
             }
+            //if a book selected then display its info
             else{
                 $book = $this->model->getBook($_GET['book']);
                 include 'view/viewbook.php';
